f7bf49ebcce1ab2ff0d9b3787b802ddafd79884e
Andrei Herrera <herreralorenz@yahoo.com>
2025-03-21 10:09:58 +0800

    added comment

--- admin/add-account-modal.php
+++ admin/add-account-modal.php
@@ -1,4 +1,5 @@
 <!-- Add Account Modal -->
+ <!-- asdasd -->
 <div class="modal fade <?php if (isset($_SESSION['show_modal']) && $_SESSION['show_modal']) echo 'show'; ?>" 
      id="exampleModal" 
      tabindex="-1" 
