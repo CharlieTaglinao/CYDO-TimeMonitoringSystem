a07807e4377c4cfed0fc49198c62cfce734dc371
Chrltgln <taglina06@gmail.com>
2025-05-09 18:23:00 +0800

    many changes

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>City Youth Development Office</title>
+    <title>The Clubhouse Network</title>
     <link rel="stylesheet" href="assets/css/bootstrap.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
